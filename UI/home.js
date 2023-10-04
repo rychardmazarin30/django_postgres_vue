7d956fe900a885ed3fd7f6dc63a4d40ab5666ef5
Rychard Mazarin <rychardnbmazarin89@gmail.com>
2023-10-03 22:06:15 -0300

    Inserção de icons na HomePage

--- UI/home.js
+++ UI/home.js
@@ -1,1 +1,9 @@
-const home = {template: `<h1>Página Inicial</h1>`};
+const home = {template: `
+
+<h2>App Techs</h2>
+<br>
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" width="150" height="150"/> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" width="150" height="150"/> <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" width="150" height="150"/>
+
+`
+};
